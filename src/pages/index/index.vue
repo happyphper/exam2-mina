30e2f70d1320907f29d52a8da43bce1f69cd7829
Baron <282584778@qq.com>
2018-10-06 20:39:07 +0800

    初始化项目，添加vant-ui

---
+++ src/pages/index/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="container">
+    <img src="/static/images/lhdx_logo.png" class="logo">
+    <h1 class="title">考评小程序</h1>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {}
+    },
+    methods: {}
+  }
+</script>
+<style scoped>
+  .logo {
+    width: 400rpx;
+    height: 400rpx;
+  }
+  .title {
+    font-family: "微软雅黑", Arial, Helvetica, sans-serif;
+    font-size: 0.5rem;
+    color: #000;
+    letter-spacing: 0.1rem;
+  }
+</style>
