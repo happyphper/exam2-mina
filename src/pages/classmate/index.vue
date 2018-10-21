e948bd35d776634969f9fe2bdc0d2f5a70445718
Baron <282584778@qq.com>
2018-10-21 16:25:53 +0800

    实现同班同学功能

--- src/pages/classmate/index.vue
+++ src/pages/classmate/index.vue
@@ -1,23 +1,67 @@
 <template>
   <div class="container">
-    <div class="card">
-      <img src="/static/images/avatar.png" class="avatar">
-      <div class="info">
-        <p>王宝龙</p>
-        <p class="student_id">学号：110430042</p>
+    <div class="card" v-if="!tableData.length">
+      你还没有同班同学
+    </div>
+    <div v-else>
+      <div class="card" v-for="classmate in tableData" :key="classmate.id">
+        <img :src="classmate.avatar ? classmate.avatar : '/static/images/avatar.png'" class="avatar">
+        <div class="info">
+          <p>{{ classmate.name }}</p>
+          <p class="student_id">学号：{{ classmate.student_id }}</p>
+        </div>
       </div>
     </div>
+   
+    <van-notify id="classmate-notify" />
   </div>
 </template>
 
 <script>
+  import Notify from '@/../static/vant/notify/notify';
+  
   export default {
+    mounted() {
+      this.getClassmates()
+    },
     data() {
       return {
         tableData: []
       };
     },
-    methods: {}
+    methods: {
+      getClassmates() {
+        this.$http.get('/classmates').then(response => {
+          this.tableData = response.data
+        }).catch(err => {
+          if (!err.response) {
+            Notify({
+              text: '未知错误',
+              duration: 1000,
+              selector: '#classmate-notify',
+              backgroundColor: '#D65048'
+            });
+          } else if (err.response.status === 401) {
+            Notify({
+              text: '身份验证过期',
+              duration: 1000,
+              selector: '#classmate-notify',
+              backgroundColor: '#D65048'
+            });
+            setTimeout(() => {
+              wx.reLaunch({ url: '/pages/index/main' })
+            }, 1000)
+          } else {
+            Notify({
+              text: err.response.data.message,
+              duration: 1000,
+              selector: '#classmate-notify',
+              backgroundColor: '#D65048'
+            });
+          }
+        })
+      }
+    }
   };
 </script>
 
